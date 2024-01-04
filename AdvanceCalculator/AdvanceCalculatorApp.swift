df1ab06cb736e4e07f20d1b9e793b39508990448
Shivam Maheshwari <shmaheshwari@expediagroup.com>
2024-01-04 15:31:49 +0530

    Initial Commit

---
+++ AdvanceCalculator/AdvanceCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceCalculatorApp.swift
+//  AdvanceCalculator
+//
+//  Created by Shivam Maheshwari on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
